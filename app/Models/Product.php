216b8050d72c76dbf5b80dae291a9c805df3d81f
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2026-02-25 17:05:46 +0200

    add factory class to product table,defined fillable attributes and do some casting on price and stock_quantity columns

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'sku',
+        'name',
+        'description',
+        'price',
+        'stock_quantity',
+        'low_stock_threshold',
+        'status',
+    ];
+    
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected $casts = [
+        'price' => 'decimal:2',
+        'stock_quantity' => 'decimal:2',
+    ];
 }
---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => Str::uuid(),
+            'sku' => 'SKU-' . $this->faker->unique()->numberBetween(100000, 999999),
+            'name' => $this->faker->words(3, true),
+            'description' => $this->faker->sentence(),
+            'price' => $this->faker->randomFloat(2, 10, 1000),
+            'stock_quantity' => $this->faker->randomFloat(2, 0, 1000),
+            'low_stock_threshold' => $this->faker->numberBetween(5, 50),
+            'status' => $this->faker->randomElement(['active', 'inactive', 'discontinued'])
+        ];
+    }
+}
